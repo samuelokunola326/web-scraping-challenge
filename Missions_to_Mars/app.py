6ed8e90818a7ee04ab094e9322b317cad2bf98cf
samuelokunola326 <samuelokunola@gmail.com>
2020-11-14 04:19:05 -0500

    updates to app py file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -8,3 +8,9 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
