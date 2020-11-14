b90a572a0e714af86b6be8eace04bdf0a89b74fe
samuelokunola326 <samuelokunola@gmail.com>
2020-11-14 04:19:54 -0500

    updates to app py file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -14,3 +14,12 @@ mongo = PyMongo(app)
 def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
+
+
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
